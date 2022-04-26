106764849028353c9cf1bb7f3ab701e3e7b02190
epicMonkey13 <56354271+epicMonkey13@users.noreply.github.com>
2022-04-26 16:20:56 +0100

    first commit

---
+++ src/Checkbox.js
@@ -0,0 +1,22 @@
+import { useReducer } from "react";
+
+function Checkbox() {
+  const [checked, toggle] = useReducer(
+    checked => !checked,
+    false
+  );
+
+
+  return(
+    <>
+    <input
+    type="checkbox"
+    value={checked}
+    onChange={toggle}
+    />
+    {checked ? "checked" : "not checked"}
+    </>
+  );
+}
+
+export default Checkbox;
